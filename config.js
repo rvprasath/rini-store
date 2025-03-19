539925f89d167a9c20c9c9fb1109530f7017c9be
rvprasath <vinothr648@gmail.com>
2025-03-20 04:56:32 +0530

    config updated

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 module.exports = {
     UPLOAD_DIR: process.env.UPLOAD_DIR || '/Users/macbook/Public/Shrinidhi_Project/s3',
-    LOGS_DIR: process.env.LOGS_DIR || '/Users/macbook/Public/Shrinidhi_Project/logs',
     HOST: 'localhost',
     USER: '',
     PASSWORD: '',
